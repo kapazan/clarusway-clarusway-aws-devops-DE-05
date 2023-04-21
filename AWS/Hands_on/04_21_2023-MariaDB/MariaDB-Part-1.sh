1600a9930da69d6f250b342414aeda3bdece2eb4
kerubo <48017280+aragakerubo@users.noreply.github.com>
2023-04-21 22:52:47 +0300

    MariaDB

--- AWS/Hands_on/04_21_2023-MariaDB/MariaDB-Part-1.sh
+++ AWS/Hands_on/04_21_2023-MariaDB/MariaDB-Part-1.sh
@@ -11,8 +11,8 @@
 # Connect to EC2 instance with SSH.
 
 # Update yum package management and install MariaDB server.
-sudo dnf update -y
-sudo dnf install mariadb105-server -y
+sudo yum update -y
+sudo yum install mariadb-server -y
 
 # Start MariaDB service.
 sudo systemctl start mariadb
