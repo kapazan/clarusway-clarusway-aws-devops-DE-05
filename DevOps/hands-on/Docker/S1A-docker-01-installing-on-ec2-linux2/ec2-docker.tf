bafba99af6d99c53ca10505a5f687608ef6e3bd5
oliver <oliver@clarusway.com>
2023-07-05 03:28:57 +0300

    docker tf modified

--- DevOps/hands-on/Docker/S1A-docker-01-installing-on-ec2-linux2/ec2-docker.tf
+++ DevOps/hands-on/Docker/S1A-docker-01-installing-on-ec2-linux2/ec2-docker.tf
@@ -55,13 +55,13 @@ resource "aws_instance" "tf-ec2" {
   user_data = <<-EOF
               #!/bin/bash
               hostnamectl set-hostname docker_instance
-              yum update -y
-              yum install docker -y
+              dnf update -y
+              dnf install docker -y
               systemctl start docker
               systemctl enable docker
               usermod -a -G docker ec2-user
               # install docker-compose
-              curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+              curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
 	          EOF
 }  
